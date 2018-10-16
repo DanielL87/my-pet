06b25858141034f716de8e7be3d1b54f96e16e78
Guest User <Guest@epicodus-12.local>
2018-10-16 11:01:00 -0700

    styled

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background")
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background")
+  });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+
+  });
+
+});
